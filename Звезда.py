159932e9ab5490fd54ccf8feac3613d69afbb1fc
ValentinChernyaev <sergeich1997@yandex.ru>
2016-09-20 23:57:55 +0500

    Add files via upload

---
+++ Звезда.py
@@ -0,0 +1,27 @@
+﻿# Звезда
+# Алгоритм https://sites.google.com/site/logomiry3/4
+#
+
+# Импорт библиотеки
+from turtle import *
+
+n1=0.0
+
+n=int(input("Ввведите максимальное количество лучей( не менее 3)"))
+
+shape ('arrow')
+screensize(800,800)
+color('red', 'yellow')
+pensize(1)
+speed('fast')
+
+begin_fill() # пробуем покрасить с заливкой
+
+
+
+for i in range (0,n):
+        forward (200) # длина отрезка
+        left (360*4/n) # угол
+
+end_fill() # хватит лить краску
+done() # функция окончания рисования
---
+++ Спираль.py
@@ -0,0 +1,27 @@
+﻿#Спираль
+
+# Импорт библиотеки
+from turtle import *
+
+
+
+n=int(input("Ввведите число витков спирали (не более 10)"))
+
+shape ('arrow')
+screensize(800,800)
+color('red', 'red')
+pensize(3)
+speed('fast')
+
+begin_fill()
+penup()
+setposition(380, 380)
+pendown()
+for i in range(0,2*n) : # всего нужно 2*n секций
+    for j in range(1,3): # фрагмент спирали из 3 секций
+        backward (400-20*i)
+        left(90)
+
+
+
+done()
