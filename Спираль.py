6f650fe9840f68f83021c6c07c61da0ec1a2d3fe
ValentinChernyaev <sergeich1997@yandex.ru>
2016-09-21 01:07:02 +0500

    Add files via upload

---
+++ Спираль.py
@@ -0,0 +1,25 @@
+﻿
+from turtle import *
+
+
+
+n=int(input("Ввведите число витков спирали (не более 10)"))
+
+shape ('arrow')
+screensize(800,800)
+color('red', 'red')
+pensize(3)
+speed('fast')
+
+begin_fill()
+penup()
+setposition(380, 380)
+pendown()
+for i in range(0,2*n) :
+    for j in range(1,3):
+        backward (400-20*i)
+        left(90)
+
+
+
+t.done()
