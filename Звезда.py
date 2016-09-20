3118cbf5b135179611e59f4d026b04252b59bcca
ValentinChernyaev <sergeich1997@yandex.ru>
2016-09-21 00:07:43 +0500

    Лаба 2

--- Звезда.py
+++ Звезда.py
@@ -1,27 +1,20 @@
-﻿# Звезда
-# Алгоритм https://sites.google.com/site/logomiry3/4
+#-------------------------------------------------------------------------------
+# Name:        модуль1
+# Purpose:
 #
-
-# Импорт библиотеки
+# Author:      Черняев Валентин
+#
+# Created:     20.09.2016
+# Copyright:   (c) Черняев Валентин 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
 from turtle import *
-
-n1=0.0
-
-n=int(input("Ввведите максимальное количество лучей( не менее 3)"))
-
-shape ('arrow')
-screensize(800,800)
 color('red', 'yellow')
-pensize(1)
-speed('fast')
-
-begin_fill() # пробуем покрасить с заливкой
-
-
-
-for i in range (0,n):
-        forward (200) # длина отрезка
-        left (360*4/n) # угол
-
-end_fill() # хватит лить краску
-done() # функция окончания рисования
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
---
+++ Многоугольник1.py
@@ -0,0 +1,28 @@
+﻿
+from turtle import *
+
+
+
+n=int(input("Ввведите максимальное количество углов( не менее 3)"))
+
+shape ('arrow')
+screensize(800,800)
+color('red')
+pensize(3)
+speed('fast')
+
+begin_fill()
+penup()
+setposition(0, -380)
+pendown()
+
+
+for i in range (3,n+1):
+
+
+ for j in range(0,i) :
+        forward (200)
+        left (360/i)
+
+
+done()
